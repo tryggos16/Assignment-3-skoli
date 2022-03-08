f6a67c87b2cac2bce0d64bbaeb660f4e01b5bd86
tryggos16 <tryggvi.bogason@gmail.com>
2022-03-08 14:10:03 +0000

    Update verk_3_s2.dart

--- verk_3_s2.dart
+++ verk_3_s2.dart
@@ -1,2 +1,12 @@
 import "dart:io";
 
+void main() {
+  print("what number do you want to multiply? ");
+  int input = int.parse(stdin.readLineSync());
+
+  for(int i = 0; i<=10; i++) {
+    int sum = input * i;
+    print("$input * $i = " + sum.toString());
+
+  }
+}
